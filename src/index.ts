5838036afd81708413811904a4b88d1c0ab1822f
Craig Wurld <85875550+dCOMMICS@users.noreply.github.com>
2023-04-26 16:50:59 +0300

    tYPEsCRIPT

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+
+//get user info//
+interface User {
+    id: number
+    firstName: string
+    lastName: string
+    role: string
+  }
+   
+  function updateUser(id: number, update: Partial<User>) {
+    const user = getUser(id)
+    const newUser = { ...user, ...update }
+    saveUser(id, newUser)
+  }
+   
